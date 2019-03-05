5cca306aac97472391737a060ab5b9495647c4db
aofleejay <aofleejay@hotmail.com>
2019-03-05 11:27:45 +0700

    :sparkles: Add versioning endpoint.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,10 @@
 import { Router } from 'express'
-import expressRoutesVersioning from 'express-routes-versioning'
-import {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-} from '../controllers/user'
+import * as v1 from './v1'
+import * as v2 from './v2'
 
 const router = Router()
-const routesVersioning = expressRoutesVersioning()
 
-router.use((req, res, next) => {
-  req.version = req.header('accept-version') || '2.0.0'
-  next()
-})
-
-const APIVersionNotExist = (req, res) => {
-  res.formatter.notFound([
-    { message: 'API version not exist.' },
-  ])
-}
-
-router.get('/users', routesVersioning({
-  "1.0.0": getUsers,
-  "2.0.0": getUsers,
-}, APIVersionNotExist))
-router.get('/users/:id', getUserById)
-router.post('/users', createUser)
-router.put('/users/:id', updateUser)
-router.delete('/users/:id', deleteUser)
+router.use('/v1', v1.user)
+router.use('/v2', v2.user)
 
 export default router
